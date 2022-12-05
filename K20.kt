0e7c2d45851f634c334cf660a33f54a32a08617d
Lucifer <57283974+H1m9n5hu@users.noreply.github.com>
2022-12-06 04:23:06 +0530

    Battery Low

---
+++ K20.kt
@@ -0,0 +1,9 @@
+fun main(){
+    repeat(readLine()!!.toInt()){
+        val x = readLine()!!.toInt()
+        if(x <= 15)
+            println("Yes")
+        else
+            println("No")
+    }
+}
