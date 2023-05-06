5af9a11618dc3e2129032df2577d0bb823520bbb
Lucifer <57283974+H1m9n5hu@users.noreply.github.com>
2023-05-06 22:26:24 +0530

    Battery Health

---
+++ K04.kt
@@ -0,0 +1,9 @@
+fun main(){
+    repeat(readLine()!!.toInt()){
+        var x = readLine()!!.toInt()
+        if(x >= 80)
+            println("Yes")
+        else
+            println("No")
+    }
+}
