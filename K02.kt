52434caf7ed2bc92d0451689fe4821c8284ec438
Lucifer <57283974+H1m9n5hu@users.noreply.github.com>
2023-06-10 00:37:38 +0530

    Delete K02.kt

--- K02.kt
+++
@@ -1,9 +0,0 @@
-fun main(){
-    repeat(readLine()!!.toInt()){
-        var (x, y) = readLine()!!.split(" ").map(String::toInt)
-        if(x+y > 6)
-            println("Yes")
-        else
-            println("No")
-    }
-}
