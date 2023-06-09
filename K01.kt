daa3ab4654d1a2332d82915f8be820c38f5ebdf5
Lucifer <57283974+H1m9n5hu@users.noreply.github.com>
2023-06-10 00:15:13 +0530

    Delete K01.kt

--- K01.kt
+++
@@ -1,9 +0,0 @@
-fun main(){
-    repeat(readLine()!!.toInt()){
-        val x = readLine()!!.toInt()
-        if(x > 0 && x <= 10)
-            println("Yes")
-        else
-            println("No")
-    }
-}
